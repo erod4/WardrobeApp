101e88dcbe05497517267a7726ddfcd508a9aea0
Enrique  Rodriguez <erod.71@outlook.com>
2024-04-24 20:49:22 -0700

    database config

--- Server/Config/dbConnect.js
+++ Server/Config/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://erod164:2dDNOGTsyTvLOpFv@cluster0.a23uplj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      `mongodb+srv://erod164:${process.env.MONGO_DB_PASSWORD}@cluster0.a23uplj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("> Connected To Database");
   } catch (error) {
